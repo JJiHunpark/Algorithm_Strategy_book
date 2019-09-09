30c1c3959a12ad3147262ca860b3d7b73a90276f
jihunparkme <jihunpark.tech@gmail.com>
2019-09-09 17:42:50 +0900

    Add: Add .java file
    Signed-off-by: jihunparkme <jihunpark.tech@gmail.com>

--- 7.divid&conquer/C7_1.java
+++ 7.divid&conquer/C7_1.java
@@ -1,9 +1,12 @@
-
 public class C7_1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int fastSum(int num) {
+		if(num == 1) return 1;
+		// 홀수일 경우, 짝수인 n-1번째 까지의 합을 재귀 호출로 구한 후 마지막에 num을 더해줌
+		if(num % 2 == 1) return fastSum(num-1) + num;	
+		// 짝수일 경우 더 작은 부분으로 분할하여 재귀 호출
+		return 2*fastSum(num/2) + (num/2)*(num/2);	
 	}
-
+	public static void main(String[] args) {
+		System.out.println(fastSum(10));
+	}
 }
