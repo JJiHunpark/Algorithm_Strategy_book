a30a88f6b39641898516b021edfb10b2e38bcf74
jihunparkme <jihunpark.tech@gmail.com>
2019-09-15 01:07:50 +0900

    Code: solve a problem

--- 7.divid&conquer/C7_5_QUADTREE.java
+++ 7.divid&conquer/C7_5_QUADTREE.java
@@ -1,17 +1,23 @@
-﻿import java.io.*;
+import java.io.*;
 
 public class C7_5_QUADTREE {
 	public static int C, position;
 	
 	public static String reverse(String quadTree) {
-		// quadtree의 첫 글자가 x가 아닐 경우 해당 코드를 리턴 
-		if(quadTree.charAt(position) != 'x') {	
+		// quadtree의 첫 글자가 x가 아닐 경우 해당 압축 코드를 리턴
+		if(quadTree.charAt(position) != 'x') {
 			position++;
 			return quadTree.charAt(position-1) + "";
 		}
-		else
-		// 첫 글자가 x일 경우
-			
+		// quadtree의 첫 글자가 x일 경우 재귀 호출 수행
+		position++;
+		String upperLeft = reverse(quadTree);	// 1
+		String upperRight = reverse(quadTree);	// 2
+		String lowerLeft = reverse(quadTree);	// 3
+		String lowerRight = reverse(quadTree);	// 4
+		
+		// 상하로 뒤집은 결과 3,4,1,2
+		return "x"+ lowerLeft + lowerRight + upperLeft + upperRight;
 	}
 	
 	public static void run() throws IOException {
@@ -28,7 +34,7 @@ public class C7_5_QUADTREE {
 			assert quadTree.length() <= 1000 : "Error :: quadtree must be no more than 1,000.";
 			
 			position = 0;
-			bw.write(reverse(quadTree));
+			bw.write(reverse(quadTree) + "\n");
 		}
 		bw.close();
 		
